3ddedc7b45d535f3fc93317e8bd6aeb853abf14b
GergoRadeczki <g.radeczki@gmail.com>
2018-10-20 21:27:17 +0200

    the game is now a game in some way

--- jatekok/teszt-jatek/script.js
+++ jatekok/teszt-jatek/script.js
@@ -1,14 +1,16 @@
-var canvas, ctx;
+var canvas, ctx, pattern;
 var requestId = 0;
 var width = 0;
 var height = 0;
 var point = [60,60];
+var score = 0;
 var currentSpeed = 0;
 var maxSpeed = 10;
 var startTime = 0;
 var i = 0;
 var q = 0;
 var d = 0;
+var coins = [1,1,1,1,1,1,1];
 
 var keys = {
     w: false,
@@ -16,9 +18,12 @@ var keys = {
     a: false,
     d: false
 };
+var coinPickupEffect = new Audio("sound-effect/coin-pickup.wav");
+var coin = new Image();
+coin.src = "img/coin-sprite.png";
 
-var img = new Image();
-img.src = "img/coin-sprite.png";
+var background = new Image();
+background.src = "img/background-grass.png";
 
 var player = new Image();
 player.src = "img/character-sprite-mirrored.png";
@@ -33,6 +38,7 @@ function startup() {
     canvas.width = width;
     canvas.height = height;
     ctx = canvas.getContext("2d");
+    pattern = ctx.createPattern(background, 'repeat');
 
     requestAnimationFrame(draw);
 }
@@ -41,7 +47,7 @@ function draw() {
 
     ctx.beginPath()
     ctx.rect(0,0,width,height);
-    ctx.fillStyle = "grey";
+    ctx.fillStyle = pattern;
     ctx.fill();
 
     currentSpeed = maxSpeed * (Date.now() - startTime)/350;
@@ -134,12 +140,25 @@ function draw() {
     //  kv = kivágás vége
     //  drawImage(img,   kkX, kkY, kvX, kvY, pozX, pozY,    mérX, mérY,)
 
-    ctx.drawImage(img,100*i,0,100,100,500,500,50,50);
-    ctx.drawImage(img,100*i,0,100,100,400,500,50,50);
-    ctx.drawImage(img,100*i,0,100,100,300,500,50,50);
-    ctx.drawImage(img,100*i,0,100,100,200,500,50,50);
-    ctx.drawImage(img,100*i,0,100,100,100,500,50,50);
-    
+    for(r = 1; r < 8; r++) {
+        if(130*r - 40 < point[0] && point[0] <= 130*r + 35 && 500-55 < point[1] && point[1] <= 500+40 && coins[r-1] == 1) {
+            //console.log(r + ". érmét megérintetted!");
+            coinPickupEffect.pause();
+            coinPickupEffect.currentTime = 0;
+            coinPickupEffect.play();
+            coins[r-1] = 0;
+            score++;
+            document.getElementById("scoreboard").innerHTML = score + " Pont";
+        }
+        if(coins[r-1] == 1) {
+            ctx.drawImage(coin, 100*i,0, 100,100, r*130,500, 50,50);
+        }
+    }
+    /*ctx.drawImage(coin, 100*i,0, 100,100, 130,500 ,50,50);
+    if(130-40 < point[0] && point[0] <= 130+35 && 500-55 < point[1] && point[1] <= 500+40) {
+        console.log("1. érmét megérintetted!");
+    }*/
+
     if(keys["w"] == true || keys["a"] == true || keys["s"] == true || keys["d"] == true) {
         ctx.drawImage(player, 256*q,0, 256,218, point[0],point[1], 60,60);
     } else {

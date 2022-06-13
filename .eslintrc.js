d0d83cf48312bb7000ba142f0fbf1eab2438aa72
Gergő Radeczki <g.radeczki@gmail.com>
2022-06-13 12:05:38 +0200

    Added Gatsby with ESLint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  globals: {
+    __PATH_PREFIX__: true,
+  },
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
